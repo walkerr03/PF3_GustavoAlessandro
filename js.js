c7d551bcf524b3d871729d9d0e8d9c69c224f876
walkerr03 <80414904+walkerr03@users.noreply.github.com>
2021-11-19 23:44:08 -0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,7 @@
+var button = document.querySelector(".b1");
+
+
+button.onclick = function() {
+ window.open('https://previg.org.br/2020/12/04/brasileiro-no-tem-o-hbito-de-poupar-segundo-pesquisa/', '_blank');
+
+}
